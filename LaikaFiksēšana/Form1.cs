d05bbac8c7745c0e58658c8b9ec7b2a2566c173e
Stilbss <121640826+Stilbss@users.noreply.github.com>
2023-01-12 18:16:06 +0200

    laikafiksēšana

---
+++ LaikaFiksēšana/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaikaFiksēšana
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(timer_Tick);
+            string laiks = DateTime.Now.ToString();
+            label1.Text = ("Programma tika atvērta :" + laiks);
+            timer.Start();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        
+        {
+            
+
+        }
+    }
+}
